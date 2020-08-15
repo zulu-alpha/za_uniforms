f6179edb04e056b332fd44aee2f219fe6c0631da
Adam Piskorski <adam@piskorski.me>
2020-08-15 17:38:36 +0200

    refactor: Move files to ZA standard

--- @zamo_uniforms/mod.cpp
+++ @zamo_uniforms/mod.cpp
@@ -1,18 +1,18 @@
 /// Name of your mod
-name = "Zulu-Alpha Uniforms";
-author = "Zulu-Alpha Mods Office";
+name = "Zulu-Alpha - Uniforms";
+author = "Zulu-Alpha - Mods Office";
 /// Picture displayed from the expansions menu/ Optimal size is 2048x1024, other sizes work too
-picture = "Icon\Logo.paa";
+picture = "Data\picture.paa";
 /// Display next to the item added by the mod
-logoSmall = "Icon\LogoSmall.paa";
+logoSmall = "Data\logo_small.paa";
 /// Logo displayed in the main menu
-logo = "Icon\Logo.paa";
+logo = "Data\logo.paa";
 /// When the mouse is over, in the main menu
-logoOver = "Icon\LogoSmallOver.paa";
+logoOver = "Data\logo_over.paa";
 /// Website URL, that can accessed from the expansions menu
-action = "http://zulu-alpha.co.za/";
+action = "https://github.com/zulu-alpha/zam_uniforms";
 /// Tool tip displayed when the mouse is left over, in the main menu
-tooltipOwned = "Zulu-Alpha Uniforms";
+tooltipOwned = "Zulu-Alpha - Uniforms";
 /// Color used for DLC stripes and backgrounds (RGBA)
 dlcColor[] =
 	{
