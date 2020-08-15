feb19e9256167e25b831f74604e36ad11aaa760d
Adam Piskorski <adam@piskorski.me>
2020-08-15 17:18:03 +0200

    docs: Update author

--- source/mod.cpp
+++ source/mod.cpp
@@ -1,6 +1,6 @@
 /// Name of your mod
 name = "Zulu-Alpha Uniforms";
-author = Zulu-Alpha Bakkies
+author = "Zulu-Alpha Mods Office";
 /// Picture displayed from the expansions menu/ Optimal size is 2048x1024, other sizes work too
 picture = "Icon\Logo.paa";
 /// Display next to the item added by the mod
@@ -15,12 +15,11 @@ action = "http://zulu-alpha.co.za/";
 tooltipOwned = "Zulu-Alpha Uniforms";
 /// Color used for DLC stripes and backgrounds (RGBA)
 dlcColor[] =
-{
-	0.23,
-	0.39,
-	0.30,
-	1
-};
+	{
+		0.23,
+		0.39,
+		0.30,
+		1};
 /// Overview text, displayed from the extension menu
 overview = "Zulu-Alpha Uniform mod";
 /// Hide the extension name
